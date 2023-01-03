cd5b2c73ffc02d79d7ccd19fbdaff8481f06f58d
KiLimePie <121055670+KiLimePie@users.noreply.github.com>
2023-01-03 13:54:17 -0700

    Add files via upload

---
+++ NameChange.py
@@ -0,0 +1,31 @@
+f=open("name.txt", "r+")
+
+count=1
+try: 
+  while True:
+    contents=f.readline().strip().split()
+    
+    fname=contents[0]
+    lname=contents[-1]
+    
+    first=(fname + lname)
+    second=(fname[0] + lname)
+    third=(fname+lname[0])
+    fourth=(fname+"."+lname)
+    
+    with open("NewName.txt", 'a+') as b:
+      b.write(f"""Name #{count}
+
+{first}
+{second}
+{third}
+{fourth}
+
+""")
+      count+=1
+    if contents=="":
+      break
+  f.close()
+  b.close()
+except:
+  print("The script has completed. Check file for results.")
